87bfb74194b46a8e628bd6d3f492dd3f85d23ae2
Hugo Egerström <Hugo.Egerstrom@alten.se>
2020-01-15 14:35:26 +0100

    Add support for Olimex  Shield LCD16x2 with 4 buttons Two modes, trigger and interval Button 1: Mode Button 2: + Button 3: - Button 4: Start

--- ND40intervalometer/ND40intervalometer.ino
+++ ND40intervalometer/ND40intervalometer.ino
@@ -1,38 +1,90 @@
-#include <avr/sleep.h>
+#include <LCD16x2.h>
+#include <Wire.h>
+//#include <avr/sleep.h>
+
+// Using Olimex Shield LCD16x2 with 4 buttons
+// https://www.electrokit.com/produkt/lcd-knapp-shield-i2c-uart/
+LCD16x2 lcd;
+int buttons;
 int IRledPin =  13;
-int buttonPin = 2;
-int buttonState = 0;         // variable for reading the pushbutton status
+
+#define interval_mode 0
+#define trigger_mode 1
+int mode = trigger_mode;
+int interval_seconds = 1;
+bool updateDisplay = true;
 
 void setup() {
   pinMode(IRledPin, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  Wire.begin();
+  delay(500); //Wait for communcations to establish
+  lcd.lcdSetBlacklight(128);
+  displayState();
 }
 
 void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-  
-  if (buttonState == HIGH) {
-    LedSequence(LED_BUILTIN);
-    takePhoto(IRledPin);
+  buttons = lcd.readButtons();
+
+  if (~buttons & 0x01) // Mode
+  {
+    if (mode == interval_mode)
+      mode = trigger_mode;
+    else if (mode == trigger_mode)
+      mode = interval_mode;
+    updateDisplay = true;
   }
-  //delay(30000);
+
+  if (~buttons & 0x02) //+
+  {
+    interval_seconds++;
+    updateDisplay = true;
+  }
+
+  if (~buttons & 0x04) //-
+  {
+    if(interval_seconds >= 2)
+    {
+      interval_seconds--;
+      updateDisplay = true;
+    }
+  }
+
+  if (~buttons & 0x08) // Start
+  {
+    if (mode == trigger_mode)
+      takePhoto(IRledPin);
+  }
+
+  displayState();
+  delay(100);
 }
 
-void LedSequence(int led) {
-  int blinks = 3;
-
-  while (blinks > 0) {
-    digitalWrite(led, HIGH);
-    delay(200);
-    digitalWrite(led, LOW);
-    delay(200);
-    blinks--;
+void displayState()
+{
+  if (!updateDisplay) {
+    return;
   }
+
+  lcd.lcdClear();
+  lcd.lcdGoToXY(1, 1);
+  lcd.lcdWrite("Nikon D40 Hugo E");
+  lcd.lcdGoToXY(1, 2);
+  if (mode == interval_mode) {
+    lcd.lcdWrite("Interval ");
+    lcd.lcdGoToXY(10, 2);
+    char buffer[2];
+    itoa(interval_seconds, buffer, 10);
+    lcd.lcdWrite(buffer);
+    lcd.lcdGoToXY(13, 2);
+    lcd.lcdWrite("sec.");
+  }
+  else if (mode == trigger_mode)
+    lcd.lcdWrite("Trigger");
+  updateDisplay = false;
 }
 
-//Nikon D40 sequence (source: https://www.christidis.info/index.php/personal-projects/arduino-nikon-infrared-command-code)
+// Nikon D40 sequence
+// source: https://www.christidis.info/index.php/personal-projects/arduino-nikon-infrared-command-code)
 void takePhoto(int led) {
   int i;
   for (i = 0; i < 76; i++) {
