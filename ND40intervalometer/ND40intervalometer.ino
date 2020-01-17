2230835420efe7def2891bff5ea9d9dd0e7d656c
Hugo Egerström <hugo@egerstrom.se>
2020-01-17 14:05:43 +0100

    Version 1.0
    Fix bugs
    Refactor names
    Add a loop for time lapse

--- ND40intervalometer/ND40intervalometer.ino
+++ ND40intervalometer/ND40intervalometer.ino
@@ -1,6 +1,5 @@
 #include <LCD16x2.h>
 #include <Wire.h>
-//#include <avr/sleep.h>
 
 // Using Olimex Shield LCD16x2 with 4 buttons
 // https://www.electrokit.com/produkt/lcd-knapp-shield-i2c-uart/
@@ -10,14 +9,16 @@ int IRledPin =  13;
 
 #define interval_mode 0
 #define trigger_mode 1
-#define interval_started_mode 2
-int mode = trigger_mode;
-
+#define time_lapse_mode 2
+int mode = interval_mode;
 
 long intervals[12] = {1000, 2000, 4000, 8000, 15000, 30000, 45000, 60000, 120000, 240000, 480000, 840000};
 int interval_index = 0;
 #define intervals_min 0
 #define intervals_max 11
+#define interval_timing_compensation 35
+
+int exposureCounter = 0;
 
 void setup() {
   pinMode(IRledPin, OUTPUT);
@@ -28,53 +29,72 @@ void setup() {
 }
 
 void loop() {
-  buttons = lcd.readButtons();
-
-  if (~buttons & 0x01) // Mode
+  if (mode == time_lapse_mode)
   {
-    if (mode == interval_mode)
-      mode = trigger_mode;
-    else if (mode == trigger_mode)
-      mode = interval_mode;
-    updateDisplayMode();
+    takePhoto(IRledPin);
+    exposureCounter++;
+    updateDisplayCounter();
+    delay(intervals[interval_index] - interval_timing_compensation);
   }
-
-  if (~buttons & 0x02) // +
+  else
   {
-    interval_index++;
-    if(interval_index > intervals_max)
-      interval_index = intervals_min;
-    updateDisplayInterval();
-  }
-
-  if (~buttons & 0x04) // -
-  {
-    interval_index--;
-    if(interval_index < intervals_min)
-      interval_index = intervals_max;
-    updateDisplayInterval();
-  }
-
-  if (~buttons & 0x08) // Start
-  {
-    if (mode == trigger_mode)
+    buttons = lcd.readButtons();
+    if (~buttons & 0x01) // Mode
     {
-      takePhoto(IRledPin);
+      if (mode == interval_mode)
+        mode = trigger_mode;
+      else if (mode == trigger_mode)
+        mode = interval_mode;
+      updateDisplayMode();
     }
-    else if (mode == interval_mode)
+
+    if (~buttons & 0x02) // +
     {
-      mode = interval_started_mode;
-      lcd.lcdSetBlacklight(0);
+      interval_index++;
+      if (interval_index > intervals_max)
+        interval_index = intervals_min;
+      updateDisplayInterval();
     }
+
+    if (~buttons & 0x04) // -
+    {
+      interval_index--;
+      if (interval_index < intervals_min)
+        interval_index = intervals_max;
+      updateDisplayInterval();
+    }
+
+    if (~buttons & 0x08) // Start
+    {
+      if (mode == trigger_mode)
+      {
+        takePhoto(IRledPin);
+        exposureCounter++;
+        updateDisplayCounter();
+      }
+      else if (mode == interval_mode)
+      {
+        lcd.lcdSetBlacklight(0);
+        lcd.lcdClear();
+        lcd.lcdGoToXY(1, 1);
+        lcd.lcdWrite("Interval");
+        updateDisplayInterval();
+        lcd.lcdGoToXY(1, 2);
+        lcd.lcdWrite("Counter");
+        exposureCounter = 0;
+        mode = time_lapse_mode;
+      }
+    }
+    delay(100);
   }
-  delay(100);
 }
 
 void initDisplay()
 {
   lcd.lcdClear();
   lcd.lcdGoToXY(1, 1);
-  lcd.lcdWrite(">Interval  1sec.");
+  lcd.lcdWrite(">Interval");
+  updateDisplayInterval();
   lcd.lcdGoToXY(1, 2);
   lcd.lcdWrite(" Trigger");
 }
@@ -102,17 +122,24 @@ void updateDisplayInterval()
   lcd.lcdGoToXY(11, 1);
   char* unit = "sec.";
   int secondsOrMinutes = intervals[interval_index] / 1000;
-  if(secondsOrMinutes >=60)
+  if (secondsOrMinutes >= 60)
   {
     secondsOrMinutes = secondsOrMinutes / 60;
     unit = "min.";
   }
-  char buffer[2];
-  sprintf(buffer, "%2d", secondsOrMinutes);
-  //itoa(interval_seconds, buffer, 10);
-  lcd.lcdWrite(buffer);
+  char tmp[2];
+  sprintf(tmp, "%2d", secondsOrMinutes);
+  lcd.lcdWrite(tmp);
   lcd.lcdGoToXY(13, 1);
   lcd.lcdWrite(unit);
+}
+
+void updateDisplayCounter()
+{
+  lcd.lcdGoToXY(12, 2);
+  char tmp[5];
+  sprintf(tmp, "%5d", exposureCounter);
+  lcd.lcdWrite(tmp);
 }
 
 // Nikon D40 sequence
